41e784a908cd4fbd5462727a3536c251c397ede3
itzarushi16 <thearushi16@gmail.com>
2025-07-31 01:01:34 +0530

    bill generator

--- src/SellMedicine.java
+++ src/SellMedicine.java
@@ -363,6 +363,10 @@ public String getUniqueId(String prefix){
                     String a = cartTable.getValueAt(i, 0).toString();
                     String b = cartTable.getValueAt(i, 1).toString();
                     String c = cartTable.getValueAt(i, 2).toString();
+                     String d = cartTable.getValueAt(i, 3).toString();
+    String e = cartTable.getValueAt(i, 4).toString();
+    String f = cartTable.getValueAt(i, 5).toString();
+}
                 
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(null, e);
